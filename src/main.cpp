3d9239e2db9f86bd017632a2c5251b14e9f6e23f
Geoff Langdale <geoff.langdale@gmail.com>
2019-04-30 16:42:10 +1000

    Noodling around; add an example, fix some things.

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,150 @@
 #include "io_util.h"
 #include "timing.h"
 
+#include "portability.h"
 using namespace std;
+
+
+struct ParsedCSV {
+  uint32_t n_indexes{0};
+  uint32_t *indexes; 
+};
+
+struct simd_input {
+#ifdef __AVX2__
+  __m256i lo;
+  __m256i hi;
+#elif defined(__ARM_NEON)
+  uint8x16_t i0;
+  uint8x16_t i1;
+  uint8x16_t i2;
+  uint8x16_t i3;
+#else
+#error "It's called SIMDcsv for a reason, bro"
+#endif
+};
+
+really_inline simd_input fill_input(const uint8_t * ptr) {
+  struct simd_input in;
+#ifdef __AVX2__
+  in.lo = _mm256_loadu_si256(reinterpret_cast<const __m256i *>(ptr + 0));
+  in.hi = _mm256_loadu_si256(reinterpret_cast<const __m256i *>(ptr + 32));
+#elif defined(__ARM_NEON)
+  in.i0 = vld1q_u8(ptr + 0);
+  in.i1 = vld1q_u8(ptr + 16);
+  in.i2 = vld1q_u8(ptr + 32);
+  in.i3 = vld1q_u8(ptr + 48);
+#endif
+  return in;
+}
+
+// a straightforward comparison of a mask against input. 5 uops; would be
+// cheaper in AVX512.
+really_inline uint64_t cmp_mask_against_input(simd_input in, uint8_t m) {
+#ifdef __AVX2__
+  const __m256i mask = _mm256_set1_epi8(m);
+  __m256i cmp_res_0 = _mm256_cmpeq_epi8(in.lo, mask);
+  uint64_t res_0 = static_cast<uint32_t>(_mm256_movemask_epi8(cmp_res_0));
+  __m256i cmp_res_1 = _mm256_cmpeq_epi8(in.hi, mask);
+  uint64_t res_1 = _mm256_movemask_epi8(cmp_res_1);
+  return res_0 | (res_1 << 32);
+#elif defined(__ARM_NEON)
+  const uint8x16_t mask = vmovq_n_u8(m); 
+  uint8x16_t cmp_res_0 = vceqq_u8(in.i0, mask); 
+  uint8x16_t cmp_res_1 = vceqq_u8(in.i1, mask); 
+  uint8x16_t cmp_res_2 = vceqq_u8(in.i2, mask); 
+  uint8x16_t cmp_res_3 = vceqq_u8(in.i3, mask); 
+  return neonmovemask_bulk(cmp_res_0, cmp_res_1, cmp_res_2, cmp_res_3);
+#endif
+}
+
+
+// return the quote mask (which is a half-open mask that covers the first
+// quote in a quote pair and everything in the quote pair) 
+// We also update the prev_iter_inside_quote value to
+// tell the next iteration whether we finished the final iteration inside a
+// quote pair; if so, this  inverts our behavior of  whether we're inside
+// quotes for the next iteration.
+
+really_inline uint64_t find_quote_mask(simd_input in, uint64_t &prev_iter_inside_quote) {
+  uint64_t quote_bits = cmp_mask_against_input(in, '"');
+
+#ifdef __AVX2__
+  uint64_t quote_mask = _mm_cvtsi128_si64(_mm_clmulepi64_si128(
+      _mm_set_epi64x(0ULL, quote_bits), _mm_set1_epi8(0xFF), 0));
+#elif defined(__ARM_NEON)
+  uint64_t quote_mask = vmull_p64( -1ULL, quote_bits);
+#endif
+  quote_mask ^= prev_iter_inside_quote;
+
+  // right shift of a signed value expected to be well-defined and standard
+  // compliant as of C++20,
+  // John Regher from Utah U. says this is fine code
+  prev_iter_inside_quote =
+      static_cast<uint64_t>(static_cast<int64_t>(quote_mask) >> 63);
+  return quote_mask;
+}
+
+
+// flatten out values in 'bits' assuming that they are are to have values of idx
+// plus their position in the bitvector, and store these indexes at
+// base_ptr[base] incrementing base as we go
+// will potentially store extra values beyond end of valid bits, so base_ptr
+// needs to be large enough to handle this
+really_inline void flatten_bits(uint32_t *base_ptr, uint32_t &base,
+                                uint32_t idx, uint64_t bits) {
+  uint32_t cnt = hamming(bits);
+  uint32_t next_base = base + cnt;
+  while (bits != 0u) {
+    base_ptr[base + 0] = static_cast<uint32_t>(idx) + trailingzeroes(bits);
+    bits = bits & (bits - 1);
+    base_ptr[base + 1] = static_cast<uint32_t>(idx) + trailingzeroes(bits);
+    bits = bits & (bits - 1);
+    base_ptr[base + 2] = static_cast<uint32_t>(idx) + trailingzeroes(bits);
+    bits = bits & (bits - 1);
+    base_ptr[base + 3] = static_cast<uint32_t>(idx) + trailingzeroes(bits);
+    bits = bits & (bits - 1);
+    base_ptr[base + 4] = static_cast<uint32_t>(idx) + trailingzeroes(bits);
+    bits = bits & (bits - 1);
+    base_ptr[base + 5] = static_cast<uint32_t>(idx) + trailingzeroes(bits);
+    bits = bits & (bits - 1);
+    base_ptr[base + 6] = static_cast<uint32_t>(idx) + trailingzeroes(bits);
+    bits = bits & (bits - 1);
+    base_ptr[base + 7] = static_cast<uint32_t>(idx) + trailingzeroes(bits);
+    bits = bits & (bits - 1);
+    base += 8;
+  }
+  base = next_base;
+}
+
+bool find_indexes(const uint8_t * buf, size_t len, ParsedCSV & pcsv) {
+  // does the previous iteration end inside a double-quote pair?
+  uint64_t prev_iter_inside_quote = 0ULL;  // either all zeros or all ones
+  uint64_t prev_iter_cr_end = 0ULL;
+  size_t lenminus64 = len < 64 ? 0 : len - 64;
+  size_t idx = 0;
+  uint32_t *base_ptr = pcsv.indexes;
+  uint32_t base = 0;
+
+  for (; idx < lenminus64; idx += 64) {
+#ifndef _MSC_VER
+    __builtin_prefetch(buf + idx + 128);
+#endif
+    simd_input in = fill_input(buf+idx);
+    uint64_t quote_mask = find_quote_mask(in, prev_iter_inside_quote);
+    uint64_t commas = cmp_mask_against_input(in, ',') & ~quote_mask;
+    uint64_t cr = cmp_mask_against_input(in, 0x0d) & ~quote_mask;
+    uint64_t cr_adjusted = (cr << 1) | prev_iter_cr_end;
+    uint64_t lf = cmp_mask_against_input(in, 0x0a) & ~quote_mask;
+    uint64_t crlf = lf & cr_adjusted;
+    prev_iter_cr_end = cr >> 63;
+
+    uint64_t field_sep = crlf | commas;
+    flatten_bits(base_ptr, base, idx, field_sep);
+  }
+  pcsv.n_indexes = base;
+  return true;
+}
 
 int main(int argc, char * argv[]) {
   int c; 
@@ -62,17 +205,34 @@ int main(int argc, char * argv[]) {
   evts.push_back(PERF_COUNT_HW_CACHE_REFERENCES);
   evts.push_back(PERF_COUNT_HW_CACHE_MISSES);
 
+  ParsedCSV pcsv;
+  pcsv.indexes = new (std::nothrow) uint32_t[p.size()]; // can't have more indexes than we have data
+
   TimingAccumulator ta(2, evts);
   for (int i = 0; i < iterations; i++) {
     {
       TimingPhase p1(ta, 0);
+      find_indexes(reinterpret_cast<const uint8_t *>(p.data()), p.size(), pcsv);
     }
     {
       TimingPhase p2(ta, 1);
     }
   }
+
+  if (dump) {
+    for (size_t i = 0; i < pcsv.n_indexes; i++) {
+      cout << pcsv.indexes[i] << ": ";
+      if (i != pcsv.n_indexes-1) {
+        for (size_t j = pcsv.indexes[i]; j < pcsv.indexes[i+1]; j++) {
+          cout << p[j];
+        }
+      }
+      cout << "\n";
+    }
+  }
   ta.dump();
 
+  cout << "Cycles per byte " << (1.0*ta.results[0])/(iterations*p.size()) << "\n";
   if (verbose) {
     cout << "[verbose] done " << endl;
   }
---
+++ src/portability.h
@@ -0,0 +1,84 @@
+#ifndef PORTABILITY_H
+#define PORTABILITY_H
+
+#ifdef _MSC_VER
+/* Microsoft C/C++-compatible compiler */
+#include <intrin.h>
+#include <iso646.h>
+#include <cstdint>
+
+static inline bool add_overflow(uint64_t value1, uint64_t value2, uint64_t *result) {
+	return _addcarry_u64(0, value1, value2, reinterpret_cast<unsigned __int64 *>(result));
+}
+
+#pragma intrinsic(_umul128)
+static inline bool mul_overflow(uint64_t value1, uint64_t value2, uint64_t *result) {
+	uint64_t high;
+	*result = _umul128(value1, value2, &high);
+	return high;
+}
+
+
+static inline int trailingzeroes(uint64_t input_num) {
+    return _tzcnt_u64(input_num);
+}
+
+static inline int leadingzeroes(uint64_t  input_num) {
+    return _lzcnt_u64(input_num);
+}
+
+static inline int hamming(uint64_t input_num) {
+#ifdef _WIN64  // highly recommended!!!
+	return (int)__popcnt64(input_num);
+#else  // if we must support 32-bit Windows
+	return (int)(__popcnt((uint32_t)input_num) +
+		__popcnt((uint32_t)(input_num >> 32)));
+#endif
+}
+
+#else
+#include <cstdint>
+#include <cstdlib>
+
+#if defined(__BMI2__) || defined(__POPCOUNT__) || defined(__AVX2__)
+#include <x86intrin.h>
+#endif
+
+static inline bool add_overflow(uint64_t  value1, uint64_t  value2, uint64_t *result) {
+	return __builtin_uaddll_overflow(value1, value2, (unsigned long long*)result);
+}
+static inline bool mul_overflow(uint64_t  value1, uint64_t  value2, uint64_t *result) {
+	return __builtin_umulll_overflow(value1, value2, (unsigned long long *)result);
+}
+
+/* result might be undefined when input_num is zero */
+static inline int trailingzeroes(uint64_t input_num) {
+#ifdef __BMI2__
+	return _tzcnt_u64(input_num);
+#else
+	return __builtin_ctzll(input_num);
+#endif
+}
+
+/* result might be undefined when input_num is zero */
+static inline int leadingzeroes(uint64_t  input_num) {
+#ifdef __BMI2__
+	return _lzcnt_u64(input_num);
+#else
+	return __builtin_clzll(input_num);
+#endif
+}
+
+/* result might be undefined when input_num is zero */
+static inline int hamming(uint64_t input_num) {
+#ifdef __POPCOUNT__
+	return _popcnt64(input_num);
+#else
+	return __builtin_popcountll(input_num);
+#endif
+}
+
+#endif // _MSC_VER
+
+
+#endif // _PORTABILITY_H
