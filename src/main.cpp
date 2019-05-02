72e27f7c9f3b7dd8908b0c6bf37d8cdf482e5c8b
Daniel Lemire <lemire@gmail.com>
2019-05-02 13:55:49 -0400

    Loop unrolling for better pipelining.

--- src/main.cpp
+++ src/main.cpp
@@ -132,32 +132,63 @@ bool find_indexes(const uint8_t * buf, size_t len, ParsedCSV & pcsv) {
   size_t idx = 0;
   uint32_t *base_ptr = pcsv.indexes;
   uint32_t base = 0;
-
-  for (; idx < lenminus64; idx += 64) {
+  // we do the index decoding in bulk for better pipelining.
+#define SIMDCSV_BUFFERSIZE 4
+  if(lenminus64 > 64 * SIMDCSV_BUFFERSIZE) {
+    uint64_t fields[SIMDCSV_BUFFERSIZE];
+    for (; idx < lenminus64; idx += 64 * SIMDCSV_BUFFERSIZE) {
+      for(size_t b = 0; b < SIMDCSV_BUFFERSIZE; b++){
+        size_t internal_idx = 64 * b + idx;
 #ifndef _MSC_VER
-    __builtin_prefetch(buf + idx + 128);
+        __builtin_prefetch(buf + internal_idx + 128);
 #endif
-    simd_input in = fill_input(buf+idx);
-    uint64_t quote_mask = find_quote_mask(in, prev_iter_inside_quote);
-    uint64_t sep = cmp_mask_against_input(in, ',');
+        simd_input in = fill_input(buf+internal_idx);
+        uint64_t quote_mask = find_quote_mask(in, prev_iter_inside_quote);
+        uint64_t sep = cmp_mask_against_input(in, ',');
 #ifdef CRLF
-    uint64_t cr = cmp_mask_against_input(in, 0x0d);
-    uint64_t cr_adjusted = (cr << 1) | prev_iter_cr_end;
-    uint64_t lf = cmp_mask_against_input(in, 0x0a);
-    uint64_t end = lf & cr_adjusted;
-    prev_iter_cr_end = cr >> 63;
+        uint64_t cr = cmp_mask_against_input(in, 0x0d);
+        uint64_t cr_adjusted = (cr << 1) | prev_iter_cr_end;
+        uint64_t lf = cmp_mask_against_input(in, 0x0a);
+        uint64_t end = lf & cr_adjusted;
+        prev_iter_cr_end = cr >> 63;
 #else
-    uint64_t end = cmp_mask_against_input(in, 0x0a);
+        uint64_t end = cmp_mask_against_input(in, 0x0a);
+#endif
+        fields[b] = (end | sep) & ~quote_mask;
+      }
+      for(size_t b = 0; b < SIMDCSV_BUFFERSIZE; b++){
+        size_t internal_idx = 64 * b + idx;
+        flatten_bits(base_ptr, base, internal_idx, fields[b]);
+      }
+    }
+    // tail end
+    for (; idx < lenminus64; idx += 64) {
+#ifndef _MSC_VER
+      __builtin_prefetch(buf + idx + 128);
+#endif
+      simd_input in = fill_input(buf+idx);
+      uint64_t quote_mask = find_quote_mask(in, prev_iter_inside_quote);
+      uint64_t sep = cmp_mask_against_input(in, ',');
+#ifdef CRLF
+      uint64_t cr = cmp_mask_against_input(in, 0x0d);
+      uint64_t cr_adjusted = (cr << 1) | prev_iter_cr_end;
+      uint64_t lf = cmp_mask_against_input(in, 0x0a);
+      uint64_t end = lf & cr_adjusted;
+      prev_iter_cr_end = cr >> 63;
+#else
+      uint64_t end = cmp_mask_against_input(in, 0x0a);
 #endif
     // note - a bit of a high-wire act here with quotes
     // we can't put something inside the quotes with the CR
     // then outside the quotes with LF so it's OK to "and off"
     // the quoted bits here. Some other quote convention would
     // need to be thought about carefully
-    uint64_t field_sep = (end | sep) & ~quote_mask;
+      uint64_t field_sep = (end | sep) & ~quote_mask;
 
-    flatten_bits(base_ptr, base, idx, field_sep);
+      flatten_bits(base_ptr, base, idx, field_sep);
+    }
   }
+#undef SIMDCSV_BUFFERSIZE
   pcsv.n_indexes = base;
   return true;
 }
